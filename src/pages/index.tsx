78db45a4a8542d3f0b433d9aee9c02f6b89ed6e8
Pietro Campagnano <hello@pietro.camp>
2024-09-26 14:34:45 +0200

    adds remote file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Video from 'next-video';
-import getStarted from '/videos/get-started.mp4';
 
 export default function Page() {
-  return <Video src={getStarted} />;
+  return <Video src="https://file-examples.com/storage/fe58a1f07d66f447a9512f1/2017/04/file_example_MP4_480_1_5MG.mp4" />;
 }
 
